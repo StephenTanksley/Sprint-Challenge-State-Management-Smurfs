d3fcced4ac0a124145717919a0d807a139cabf05
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-26 11:55:18 -0500

    Wrote an action for the get request to the server.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,6 +13,8 @@ export const ADD_SMURF_START = "ADD_SMURFS_START"
 export const ADD_SMURF_SUCCESS = "ADD_SMURFS_SUCCESS"
 export const ADD_SMURF_FAILED = "ADD_SMURFS_FAILED"
 
+
+//STRETCH ----->
 //update
 export const UPDATE_SMURF_START = "UPDATE_SMURFS_START"
 export const UPDATE_SMURF_SUCCESS = "UPDATE_SMURFS_SUCCESS"
@@ -23,3 +25,35 @@ export const DELETE_SMURF_START = "DELETE_SMURFS_START"
 export const DELETE_SMURF_SUCCESS = "DELETE_SMURFS_SUCCESS"
 export const DELETE_SMURF_FAILED = "DELETE_SMURFS_FAILED"
 
+// <------STRETCH
+
+//For MVP, I need to create a post request. Additional stretch functionality includes full CRUD functionality.css
+
+
+export const getSmurfs = () => {
+    return dispatch => {
+        dispatch({ type: GET_SMURFS_START})
+
+        axios
+        .get('http://localhost:3333/smurfs')
+        .then(response => {
+            dispatch({GET_SMURFS_SUCCESS, payload: response.data })
+        })
+        .catch(error => {
+            dispatch ({ type: GET_SMURFS_FAILED, payload: error})
+        })
+    }
+
+}
+
+
+
+//POST request
+
+export const addSmurf = smurf => {
+    return dispatch => {
+        dispatch({ type: ADD_SMURF_START })
+
+        return axios
+    }
+}
