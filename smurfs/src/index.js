d4e1e49e8896403c92cf702d6ce85e4948e9ae36
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-26 11:25:54 -0500

    Imported package. Began to import dependencies into the index.js file.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux' 
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById("root"));
